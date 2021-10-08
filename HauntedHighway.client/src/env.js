7d1f4bcdc83604ff9316e05f06e5c970eaf73f23
Justin Carpenter <JustinCarpenter2020@Gmail.com>
2021-10-08 12:27:36 -0600

    env.js put in

--- HauntedHighway.client/src/env.js
+++ HauntedHighway.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-elomoai1.us.auth0.com'
+export const audience = 'https://sandbox'
+export const clientId = 'vgfwY74OHiK4F3aNsvcVMUOW8KmMNFzH'
