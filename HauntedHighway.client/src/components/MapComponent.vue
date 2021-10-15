9a426249f1b27c66be80e984a7cd4eab266306cd
Justin Carpenter <JustinCarpenter2020@Gmail.com>
2021-10-15 11:45:34 -0600

    marker on map

--- HauntedHighway.client/src/components/MapComponent.vue
+++ HauntedHighway.client/src/components/MapComponent.vue
@@ -9,6 +9,7 @@ import mapboxgl from 'mapbox-gl'
 export default {
   setup() {
     onMounted(() => {
+      // <---------------------------------Establishes Map --------------------------------------->
       mapboxgl.accessToken = 'pk.eyJ1Ijoiam9uZXN5amF2YSIsImEiOiJja3VyNXdiOG0wamtwMm9wandzY2dzN2JqIn0.chUhEV5TGD43wacx_ktejg'
       const map = new mapboxgl.Map({
         container: 'map', // container ID
@@ -16,6 +17,8 @@ export default {
         center: [-74.5, 40], // starting position [lng, lat]
         zoom: 9 // starting zoom
       })
+      // <----------------------------------Establishes geolocations(markerLocations)--------------------->
+      new mapboxgl.Marker().setLngLat([30.5, 50.5]).addTo(map)
     })
     return {}
   },
@@ -25,5 +28,12 @@ export default {
 
 <style lang="scss" scoped>
 #map { position: absolute; width: 95%; height: 80%; }
-
+.marker {
+  background-image: url('../assets/img/mapbox-icon.png');
+  background-size: cover;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  cursor: pointer;
+}
 </style>
