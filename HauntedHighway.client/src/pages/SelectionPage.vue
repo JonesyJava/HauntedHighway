9013d9786272a8cc3b2ed92c3cc0eb809165ebdf
Justin Carpenter <JustinCarpenter2020@Gmail.com>
2021-10-20 16:05:33 -0600

    selection cards render

--- HauntedHighway.client/src/pages/SelectionPage.vue
+++ HauntedHighway.client/src/pages/SelectionPage.vue
@@ -1,17 +1,24 @@
 <template>
-  <div class="component text-light">
-    hi selection page
+  <div class="container-fluid fixed">
+    <div class="row">
+      <SelectionCard v-for="(n, i) in arr" :key="i" />
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   setup() {
-    return {}
+    return {
+      arr: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.fixed{
+  height: 80vh;
+  overflow-y: auto ;
+}
 </style>
