5d9c84c5689be45a37e7df69815d4dd3bcb7b5ee
Justin Carpenter <JustinCarpenter2020@Gmail.com>
2021-10-21 09:37:47 -0600

    selection

--- HauntedHighway/server/db/DbContext.js
+++ HauntedHighway/server/db/DbContext.js
@@ -2,10 +2,13 @@ import mongoose from 'mongoose'
 import { AccountSchema, ProfileSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
 import BadgesSchema from '../models/Badges'
+import SelectionSchema from '../models/Selection'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
+
+  Selections = mongoose.model('Selection', SelectionSchema)
   Profiles = mongoose.model('Profile', ProfileSchema, 'accounts');
   Badges = mongoose.model('Bagde', BadgesSchema)
 }
